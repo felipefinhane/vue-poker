8389bdc40a596a0de232df63994622338f4b91a0
Felipe Finhane de Paula <felipefinhane@gmail.com>
2018-02-27 09:19:44 -0300

    New view: GameList and SerieList

--- src/views/Game/GameList.vue
+++ src/views/Game/GameList.vue
@@ -1,11 +1,77 @@
 <template>
-  <div class="animated fadeIn">
-    <p class="text-info">Partida</p>
-  </div>
+  <b-card :header="caption">
+    <button type="button" class="btn btn-primary btn-sm" href="#/player/new">Nova Partida</button>
+    <b-table :hover="hover" :striped="striped" :bordered="bordered" :small="small" :fixed="fixed" responsive="sm" :items="items" :fields="fields" :current-page="currentPage" :per-page="perPage">
+      <template slot="status" slot-scope="data">
+        <b-badge :variant="getBadge(data.item.status)">{{data.item.status}}</b-badge>
+      </template>
+    </b-table>
+    <nav>
+      <b-pagination :total-rows="getRowCount(items)" :per-page="perPage" v-model="currentPage" prev-text="Prev" next-text="Next" hide-goto-end-buttons/>
+    </nav>
+  </b-card>
 </template>
 
 <script>
-export default {
-  name: 'GameList'
-}
+  export default {
+    name: 'GameList',
+    props: {
+      caption: {
+        type: String,
+        default: 'Partidas'
+      },
+      hover: {
+        type: Boolean,
+        default: false
+      },
+      striped: {
+        type: Boolean,
+        default: false
+      },
+      bordered: {
+        type: Boolean,
+        default: false
+      },
+      small: {
+        type: Boolean,
+        default: false
+      },
+      fixed: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data: () => {
+      return {
+        items: [
+          {serie: 'Poker Dos amigos 2018/1', date: '2018/02/22', players: '9', winner: 'Pato 01', status: 'Active'},
+          {serie: 'Poker Dos amigos 2018/1', date: '2018/02/16', players: '7', winner: 'Pato 02', status: 'Done'},
+          {serie: 'Poker Dos amigos 2018/1', date: '2018/02/09', players: '8', winner: 'Pato 03', status: 'Done'},
+          {serie: 'Poker Dos amigos 2018/1', date: '2018/02/01', players: '6', winner: 'Pato 02', status: 'Done'},
+          {serie: 'Poker Dos amigos 2017/2', date: '2017/12/16', players: '11', winner: 'Pato 05', status: 'Done'}
+        ],
+        fields: [
+          {key: 'serie', label: 'Campeonato'},
+          {key: 'date', label: 'Data'},
+          {key: 'players', label: 'Participantes'},
+          {key: 'winner', label: 'Vencedor'},
+          {key: 'status'}
+        ],
+        currentPage: 1,
+        perPage: 10,
+        totalRows: 0
+      }
+    },
+    methods: {
+      getBadge (status) {
+        return status === 'Active' ? 'success'
+          : status === 'Inactive' ? 'secondary'
+            : status === 'Pending' ? 'warning'
+              : status === 'Banned' ? 'danger' : 'primary'
+      },
+      getRowCount (items) {
+        return items.length
+      }
+    }
+  }
 </script>
--- src/views/Serie/SerieList.vue
+++ src/views/Serie/SerieList.vue
@@ -1,11 +1,88 @@
 <template>
-  <div class="animated fadeIn">
-    <p class="text-info">Campeonato</p>
-  </div>
+  <b-card :header="caption">
+    <button type="button" class="btn btn-primary btn-sm" href="#/player/new">Novo Campeonato</button>
+    <b-table :hover="hover" :striped="striped" :bordered="bordered" :small="small" :fixed="fixed" responsive="sm" :items="items" :fields="fields" :current-page="currentPage" :per-page="perPage">
+      <template slot="status" slot-scope="data">
+        <b-badge :variant="getBadge(data.item.status)">{{data.item.status}}</b-badge>
+      </template>
+    </b-table>
+    <nav>
+      <b-pagination :total-rows="getRowCount(items)" :per-page="perPage" v-model="currentPage" prev-text="Prev" next-text="Next" hide-goto-end-buttons/>
+    </nav>
+  </b-card>
 </template>
 
 <script>
-export default {
-  name: 'SerieList'
-}
+  /**
+   * Randomize array element order in-place.
+   * Using Durstenfeld shuffle algorithm.
+   */
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1))
+      let temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+    return array
+  }
+
+  export default {
+    name: 'SerieList',
+    props: {
+      caption: {
+        type: String,
+        default: 'Campeonatos'
+      },
+      hover: {
+        type: Boolean,
+        default: false
+      },
+      striped: {
+        type: Boolean,
+        default: false
+      },
+      bordered: {
+        type: Boolean,
+        default: false
+      },
+      small: {
+        type: Boolean,
+        default: false
+      },
+      fixed: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data: () => {
+      return {
+        items: shuffleArray([
+          {serie: 'Poker Dos amigos 2018/1', data_inicio: '2018/02/01', data_fim: '2018/06/20', status: 'Active'},
+          {serie: 'Poker Dos amigos 2017/2', data_inicio: '2017/08/01', data_fim: '2017/12/15', status: 'Done'},
+          {serie: 'Poker Dos amigos 2017/1', data_inicio: '2017/01/26', data_fim: '2017/06/20', status: 'Done'}
+        ]),
+        fields: [
+          {key: 'serie', label: 'Campeonato'},
+          {key: 'data_inicio', label: 'Data Início'},
+          {key: 'data_fim', label: 'Data Fim'},
+          {key: 'status'}
+        ],
+        currentPage: 1,
+        perPage: 10,
+        totalRows: 0
+      }
+    },
+    methods: {
+      getBadge (status) {
+        return status === 'Active' ? 'success'
+          : status === 'Inactive' ? 'secondary'
+            : status === 'Pending' ? 'warning'
+              : status === 'Banned' ? 'danger' : 'primary'
+      },
+      getRowCount (items) {
+        return items.length
+      }
+    }
+  }
 </script>
